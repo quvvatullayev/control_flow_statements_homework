a6bb6c1eda74a030f75e9757f3c37c28d39708f7
tajriba <quvvatullayev@gmail.com>
2022-06-23 07:17:21 +0300

    if09.py

--- if09.py
+++ if09.py
@@ -9,4 +9,22 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    a1 = a % 10
+    a = a // 10
+
+    a2 = a % 10
+    a = a // 10
+
+    son = a1 * 10 + a2
+    javob = ""
+
+    if son  <= a:
+        javob += "True"
+    
+    else:
+        javob += "Fals"
+
+
+    return javob
+
+print(main(45))
